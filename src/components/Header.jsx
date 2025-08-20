3e64b6ec7e679ec94360fac5002919b7eedb9152
Muhammad Ahmad <meharahmed417@gmail.com>
2025-08-20 16:57:34 +0500

    motion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -152,7 +152,17 @@ const Header = () => {
                     transition={{ duration: 0.5 }}
                     className='fixed inset-0 bg-black/50 background-blur-sm z-50 flex items-center justify-center p-4'
                 >
-                    <div className='bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6'>
+                    <motion.div
+                    initial={{scale: 0.8, opacity: 0, y: 30}}
+                    animate={{scale:1, opacity: 1, y: 0}}
+                    exit={{scale:0.8, opacity:0, y: 30}}
+                    transition={{
+                        type:"spring",
+                        damping:30,
+                        stiffness:200,
+                        duration:0.8
+                    }}
+                    className='bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6'>
                         <div className='flex justify-between items-center mb-4'>
                             <h1 className='text-2x1 font-bold text-gray-300'>
                                 Get In Touch
@@ -169,8 +179,32 @@ const Header = () => {
                                 </label>
                                 <input type="text" id='name' placeholder='Your Name' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
                             </div>
+
+
+                            <div>
+                                <label htmlFor="email" className='block text-sm font-medium text-gray-300 mb-1'>
+                                    Email
+                                </label>
+                                <input type="email" id='email' placeholder='Your Email' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                            </div>
+
+
+                            <div>
+                                <label htmlFor="message" className='block text-sm font-medium text-gray-300 mb-1'>
+                                    Message
+                                </label>
+                                <textarea
+                                    rows="4" id='message' placeholder='How can I help you?' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                            </div>
+                            <motion.button
+                                type='submit'
+                                whileHover={{ scale: 1.03 }}
+                                whileTap={{ scale: 0.97 }}
+                                className='w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400 hover:from-violet-700 hover:to-purple-700 transition-all duration-300 rounded-lg shadow-md hover:shadow-lg hover:shadow-violet-600/50'>
+                                Send Message
+                            </motion.button>
                         </form>
-                    </div>
+                    </motion.div>
                 </motion.div>
             )}
         </header>
