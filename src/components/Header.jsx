d60ec8b4c8bfa2038e040bca41931fdba395e6e2
Muhammad Ahmad <meharahmed417@gmail.com>
2025-08-20 16:06:19 +0500

    responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 import { motion } from 'framer-motion'
-import { FiGithub, FiLinkedin, FiInstagram } from 'react-icons/fi'
+import { a, div } from 'framer-motion/client'
+import { useState } from 'react'
+import { FiGithub, FiLinkedin, FiInstagram, FiMenu, FiX } from 'react-icons/fi'
 
 const Header = () => {
+    //Toggle the menu open/close
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => setIsOpen(!isOpen);
+
+    //State to track if the contact form is open
+    const [ContactFormOpen, setContactFormOpen] = useState(false);
+
+    const openContactForm = () => setContactFormOpen(true);
+    const closeContactForm = () => setContactFormOpen(false);
     return (
         <header className='absolute w-full z-50 transition-all duration-300'>
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 md:h-20">
@@ -68,8 +79,100 @@ const Header = () => {
                         className='text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300' href="">
                         <FiInstagram className='w-5 h-5' />
                     </motion.a>
+
+                    {/* hire button*/}
+                    <motion.button
+                        onClick={openContactForm}
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{
+                            delay: 1.6,
+                            duration: 0.8,
+                            type: "spring",
+                            stiffness: 100,
+                            damping: 15,
+                        }}
+                        className='ml-4 px-4 py-2 rounded-xl bg-gradient-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-500'>
+                        Hire Me
+                    </motion.button>
+                </div>
+                {/* Mobile nav icon */}
+                <div className='md:hidden flex items-center '>
+                    <motion.button
+                        whileTap={{ scale: 0.7 }}
+                        onClick={toggleMenu}
+                        className='text-gray-300'>
+                        {isOpen ? <FiX className='h-6 w-6' /> : <FiMenu className='h-6 w-6' />}
+                    </motion.button>
                 </div>
             </div>
+            {/* Mobile Menu */}
+            <motion.div
+                initial={{ opacity: 0, height: 0 }}
+                animate={{
+                    opacity: isOpen ? 1 : 0,
+                    height: isOpen ? "auto" : 0,
+                }}
+                transition={{ duration: 0.5 }}
+                className='md:hidden overflow-hidden bg-white dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5'>
+                <nav className='flex flex-col space-y-3'>
+                    {["Home", "About", "Projects", "Experience", "Contact"].map((item) => (
+                        <a onClick={toggleMenu} className='text-gray-300 font-medium py-2' key={item} href="#">
+                            {item}
+                        </a>
+                    ))}
+                </nav>
+                <div className='pt-4 border-t border-gray-200 dark:border-gray-700'>
+                    <div className='flex space-x-5'>
+                        <a href="#">
+                            <FiGithub className='h-5 w-5 text-gray-300' />
+                        </a>
+                        <a href="#">
+                            <FiLinkedin className='h-5 w-5 text-gray-300' />
+                        </a>
+                        <a href="#">
+                            <FiInstagram className='h-5 w-5 text-gray-300' />
+                        </a>
+                    </div>
+                    <button
+                        onClick={() => {
+                            toggleMenu()
+                        }}
+                        className='mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r from-violet-600 to-violet-400 font-bold'>
+                        Contact Me
+                    </button>
+                </div>
+            </motion.div>
+            {/* Contact Form */}
+            {ContactFormOpen && (
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='fixed inset-0 bg-black/50 background-blur-sm z-50 flex items-center justify-center p-4'
+                >
+                    <div className='bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6'>
+                        <div className='flex justify-between items-center mb-4'>
+                            <h1 className='text-2x1 font-bold text-gray-300'>
+                                Get In Touch
+                            </h1>
+                            <button onClick={closeContactForm}>
+                                <FiX className='w-5 h-5 text-gray-300 font-extrabold' />
+                            </button>
+                        </div>
+                        {/* Input form */}
+                        <form className='space-y-4'>
+                            <div>
+                                <label htmlFor="name" className='block text-sm font-medium text-gray-300 mb-1'>
+                                    Name
+                                </label>
+                                <input type="text" id='name' placeholder='Your Name' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                            </div>
+                        </form>
+                    </div>
+                </motion.div>
+            )}
         </header>
     )
 }
