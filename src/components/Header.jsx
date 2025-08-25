fbe3aeec7a0a06e34faa9ea2ca9493812220d24c
Muhammad Ahmad <meharahmed417@gmail.com>
2025-08-25 16:19:56 +0500

    complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
-import { motion } from 'framer-motion'
-import { a, div } from 'framer-motion/client'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useState } from 'react'
 import { FiGithub, FiLinkedin, FiInstagram, FiMenu, FiX } from 'react-icons/fi'
 
@@ -137,6 +136,7 @@ const Header = () => {
                     <button
                         onClick={() => {
                             toggleMenu()
+                            openContactForm()
                         }}
                         className='mt-4 block w-full px-4 py-2 rounded-lg bg-gradient-to-r from-violet-600 to-violet-400 font-bold'>
                         Contact Me
@@ -144,69 +144,71 @@ const Header = () => {
                 </div>
             </motion.div>
             {/* Contact Form */}
-            {ContactFormOpen && (
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }} 
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.5 }}
-                    className='fixed inset-0 bg-black/50 background-blur-sm z-50 flex items-center justify-center p-4'
-                >
+            <AnimatePresence>
+                {ContactFormOpen && (
                     <motion.div
-                    initial={{scale: 0.8, opacity: 0, y: 30}}
-                    animate={{scale:1, opacity: 1, y: 0}}
-                    exit={{scale:0.8, opacity:0, y: 30}}
-                    transition={{
-                        type:"spring",
-                        damping:30,
-                        stiffness:200,
-                        duration:0.8
-                    }}
-                    className='bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6'>
-                        <div className='flex justify-between items-center mb-4'>
-                            <h1 className='text-2x1 font-bold text-gray-300'>
-                                Get In Touch
-                            </h1>
-                            <button onClick={closeContactForm}>
-                                <FiX className='w-5 h-5 text-gray-300 font-extrabold' />
-                            </button>
-                        </div>
-                        {/* Input form */}
-                        <form className='space-y-4'>
-                            <div>
-                                <label htmlFor="name" className='block text-sm font-medium text-gray-300 mb-1'>
-                                    Name
-                                </label>
-                                <input type="text" id='name' placeholder='Your Name' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.5 }}
+                        className='fixed inset-0 bg-black/50 background-blur-sm z-50 flex items-center justify-center p-4'
+                    >
+                        <motion.div
+                            initial={{ scale: 0.8, opacity: 0, y: 30 }}
+                            animate={{ scale: 1, opacity: 1, y: 0 }}
+                            exit={{ scale: 0.8, opacity: 0, y: 30 }}
+                            transition={{
+                                type: "spring",
+                                damping: 30,
+                                stiffness: 200,
+                                duration: 0.8
+                            }}
+                            className='bg-white dark:bg-gray-800 rounded-xl shadow-xl w-full max-w-md p-6'>
+                            <div className='flex justify-between items-center mb-4'>
+                                <h1 className='text-2x1 font-bold text-gray-300'>
+                                    Get In Touch
+                                </h1>
+                                <button onClick={closeContactForm}>
+                                    <FiX className='w-5 h-5 text-gray-300 font-extrabold' />
+                                </button>
                             </div>
+                            {/* Input form */}
+                            <form className='space-y-4'>
+                                <div>
+                                    <label htmlFor="name" className='block text-sm font-medium text-gray-300 mb-1'>
+                                        Name
+                                    </label>
+                                    <input type="text" id='name' placeholder='Your Name' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                                </div>
 
 
-                            <div>
-                                <label htmlFor="email" className='block text-sm font-medium text-gray-300 mb-1'>
-                                    Email
-                                </label>
-                                <input type="email" id='email' placeholder='Your Email' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
-                            </div>
+                                <div>
+                                    <label htmlFor="email" className='block text-sm font-medium text-gray-300 mb-1'>
+                                        Email
+                                    </label>
+                                    <input type="email" id='email' placeholder='Your Email' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                                </div>
 
 
-                            <div>
-                                <label htmlFor="message" className='block text-sm font-medium text-gray-300 mb-1'>
-                                    Message
-                                </label>
-                                <textarea
-                                    rows="4" id='message' placeholder='How can I help you?' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
-                            </div>
-                            <motion.button
-                                type='submit'
-                                whileHover={{ scale: 1.03 }}
-                                whileTap={{ scale: 0.97 }}
-                                className='w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400 hover:from-violet-700 hover:to-purple-700 transition-all duration-300 rounded-lg shadow-md hover:shadow-lg hover:shadow-violet-600/50'>
-                                Send Message
-                            </motion.button>
-                        </form>
+                                <div>
+                                    <label htmlFor="message" className='block text-sm font-medium text-gray-300 mb-1'>
+                                        Message
+                                    </label>
+                                    <textarea
+                                        rows="4" id='message' placeholder='How can I help you?' className='w-full px-4 py-2 border border-gray-600 rounded-lg focus:ring-2 focus:ring-violet-500 focus:border-violet-500 bg-gray-700' />
+                                </div>
+                                <motion.button
+                                    type='submit'
+                                    whileHover={{ scale: 1.03 }}
+                                    whileTap={{ scale: 0.97 }}
+                                    className='w-full px-4 py-2 bg-gradient-to-r from-violet-600 to-violet-400 hover:from-violet-700 hover:to-purple-700 transition-all duration-300 rounded-lg shadow-md hover:shadow-lg hover:shadow-violet-600/50'>
+                                    Send Message
+                                </motion.button>
+                            </form>
+                        </motion.div>
                     </motion.div>
-                </motion.div>
-            )}
+                )}
+            </AnimatePresence>
         </header>
     )
 }
