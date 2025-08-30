a454a2ef91096772716c8d4f44fd4c773abf8542
Muhammadahmad527 <meharahmed417@gmail.com>
2025-08-30 13:35:29 +0500

    overflow problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div>
       <Header />
       <HeroSection />
       <AboutSection />
       <CustomCursor />
-    </>
+   </div>
   ) 
 }
 
--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,10 +1,12 @@
 import {useRef , useEffect} from 'react'
 import {gsap} from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { section } from 'framer-motion/client'
 
 const AboutSection = () => {
     const sectionRef = useRef(null)
     const titleRef = useRef(null)
+    const introRef = useRef(null)
 
     useEffect(() => {
     gsap.registerPlugin(ScrollTrigger)
@@ -21,20 +23,35 @@ const AboutSection = () => {
          }   
         }
     )
+    //Intro Animation
+    gsap.fromTo(
+        introRef.current,
+        { y: 100, opacity: 0, filter: "blur(10px)"},
+        {
+            y: -400,
+            opacity: 1,
+            filter:"blur(0px)",
+            duration: 1.5,
+            scrollTrigger:{
+                trigger: sectionRef.current,
+                start: "top 40%",
+                toggleActions: "play none none reverse",
+            }
+        }
+    )
 }, [])
-console.log("✅ AboutSection is rendering!")
   return (
-    <section ref={sectionRef} className='h-screen relative overflow-auto bg-gradient-to-b from-black to-[#9a74cf50]'>
+    <section ref={sectionRef} className='h-screen relative overflow-hidden bg-gradient-to-b from-black to-[#9a74cf50]'>
         <div className='container mx-auto px-4 h-full flex flex-col items-center justify-center'>
             <h1 ref={titleRef} className='text-4xl md:text-6xl font-bold sm:mb-16 text-center text-white opacity-0'>
                 About Me
             </h1>
         </div>
-        <div>
-            <h3 className='text-sm md:text-2xl font-bold text-purple-200 z-50  lg:max-w-[45rem] max-w-[27rem] tracking-wider md:mt-20 sm:mt-[-40rem] mt-[-32rem]'>
+        <div ref={ introRef } className='absolute lg:bottom-[-20rem] md:bottom-[-10rem] bottom-[-20rem] left-0 w-full flex md:flex-row flex-col justify-between lg:px-24 px-5 items-center opacity-0'> 
+            <h3 className='text-base md:text-lg lg:text-xl text-purple-200 z-50  lg:max-w-[45rem] max-w-[27rem] tracking-wider md:mt-20 sm:mt-[-40rem] mt-[-32rem]'>
                 Hi, I'm Ahmad Shahid, a Frontend Developr focused on speed, polish, and performance. I craft responsive, user-friendly web interfaces using modern tools like React, Tailwind CSS and JavaScript. Whether it's a landing page, a full-scale web app, or something in between. I'm all about clean code, fast delivery and seamless user experiences.
             </h3>
-            <img className='lg:h-[40rem] md:h-[25rem] h-[20rem] mix-blend-lighten ' src="/images/person.png" alt="Profile-img" />
+            <img className='lg:h-[40rem] md:h-[25rem] h-[20rem] mix-blend-lighten' src="/images/person.png" alt="Profile-img" />
         </div>
     </section>
   )
