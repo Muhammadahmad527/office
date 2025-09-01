e324bdd80dc02278424a781e80928588daea0bdb
Muhammadahmad527 <meharahmed417@gmail.com>
2025-09-01 16:08:49 +0500

    complete project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect } from 'react'
+import {gsap} from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
 import Header from '../src/components/Header'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
 import CustomCursor from './components/CustomCursor'
+import ProjectSection from './components/ProjectSection'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    //Register scrolltrigger plugins
+    gsap.registerPlugin(ScrollTrigger)
+
+    //Refresh ScrollTrigger when the page is fully loaded
+    ScrollTrigger.refresh()
+
+    // Cleanup ScrollTrigger on component unmount
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill())
+    }
+  }, [])
 
   return (
     <div>
@@ -12,6 +29,7 @@ function App() {
       <HeroSection />
       <AboutSection />
       <CustomCursor />
+      <ProjectSection />
    </div>
   ) 
 }
